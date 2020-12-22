139e119aa76cc7c89f13d2cf4f0f6b7aaf1995d2
thedave42t <dave@digitalnoise.net>
2020-12-22 14:22:01 -0800

    Add tests for local datastore

---
+++ __tests__/lib/secure-local-data.test.js
@@ -0,0 +1,54 @@
+const DataStore = require('../../lib/secure-local-data');
+const fs = require('fs');
+const { encrypt, decrypt } = require('../../lib/secure');
+
+const testValue = 'This is a test string 12390389450145f @#$%!#$!@^%!#$ja:"{#@$(%!F>';
+const testKey = 'key';
+const testFile = 'datastore-test.json';
+
+test('Datastore file is successfully created', () => {
+    const ds = new DataStore(testFile);
+    expect(fs.existsSync(testFile)).toBeTruthy();
+});
+
+test('Datastore file is readable', () => {
+    const ds = new DataStore(testFile);
+    let rOk = false;
+    try {
+        fs.accessSync(testFile, fs.constants.R_OK);
+        rOk = true;
+    }
+    catch (e) {
+        rOk = false;
+    }
+    expect(rOk).toBeTruthy();
+});
+
+test('Datastore file is writeable', () => {
+    const ds = new DataStore(testFile);
+    let wOk = false;
+    try {
+        fs.accessSync(testFile, fs.constants.W_OK);
+        wOk = true;
+    }
+    catch (e) {
+        wOk = false;
+    }
+    expect(wOk).toBeTruthy();
+});
+
+test('Add a value to the datastore and make sure it\'s encrypted', () => {
+    const ds = new DataStore(testFile);
+    ds.add(testKey, testValue)    
+        .then( record => {
+            expect(record[testKey]).toBe(encrypt(testValue)); 
+        })
+});
+
+test('Get a value from the datastore and make sure it\'s decrypted', () => {
+    const ds = new DataStore(testFile);
+    ds.get(testKey)
+        .then( value => {
+            expect(value).toBe(testValue);
+        });
+});
