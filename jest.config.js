d81a5cabea01f80c6f789099a16d8a1addb9bdb3
thedave42t <dave@digitalnoise.net>
2020-12-23 11:29:42 -0800

    Limit jest to 1 worker

--- jest.config.js
+++ jest.config.js
@@ -63,12 +63,13 @@ module.exports = {
   // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
+  maxWorkers: "1",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: [
+    "node_modules",
+    "lib"
+  ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
